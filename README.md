a830607bf2e840fd9aab022a58d71e956ea46684
stvnreis <stvnreis@gmail.com>
2023-03-22 15:16:16 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## npm install nodemon@2.0.15 -D
---
+++ server.js
@@ -0,0 +1,18 @@
+const http = require('http')
+const door = 3000
+
+const route = {
+    '/': 'Curso de Node',
+    '/livros': 'Pagina de livros',
+    '/autores': 'Listagem de autores',
+    '/editoras' : 'Listagem de editoras'
+}
+
+const server = http.createServer((req, res) =>{
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(route[req.url]);
+})
+
+server.listen(door, ()=>{
+    console.log(`Server listening on http://localhost:${door}`);
+})
